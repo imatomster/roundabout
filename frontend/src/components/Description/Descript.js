a47b31eba8b89835b9cce03ff36a8925c89b6eb6
Wasabi-jpg <michael.mui09@gmail.com>
2021-07-11 15:35:00 -0700

    nav

---
+++ frontend/src/components/Description/Descript.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Descript = () => {
+    return (
+        <div>
+            <p>Welcome to Roundabout! An application to locate your closest friends & family. Easily facilitate gatherings and be up to date on arrival times.</p>
+        </div>
+    )
+}
+
+export default Descript
