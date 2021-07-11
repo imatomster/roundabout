7109ff1dd77b59fc22162ccd19db973d84e8e032
Wasabi-jpg <michael.mui09@gmail.com>
2021-07-10 17:33:54 -0700

    nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import './Navbar.css'
 
 const Navbar = () => {
     return (
         <div className="navbar-container">
-            <h1 id="title">Roundabout</h1>
+            <ul className="navbar">
+                <li className="logo"><a href="/"><h1>Roundabout</h1></a></li>
+                <li className="login"><a href="https://youtu.be/dQw4w9WgXcQ"><h2>Login</h2></a></li> 
+                <li className="notif"><a href="https://youtu.be/dQw4w9WgXcQ"><h2>Notifications</h2></a></li>
+                <li className="about"><a href="/"><h2>About</h2></a></li>
+                <li className="friends"><a href="/"><h2>Friends</h2></a></li>
+               
+    
+            </ul>
 
-            <div className="links"> 
-                
-
-            </div>
+            
         </div>
 
     )
