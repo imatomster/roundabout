8da239b7737c26ffb642c3e2eccb1e6169454ce0
Wasabi-jpg <michael.mui09@gmail.com>
2021-07-11 14:26:53 -0700

    nav

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -5,15 +5,17 @@ const Navbar = () => {
     return (
         <div className="navbar-container">
                 <div className="logo"><a href="/"><h1>Roundabout</h1></a></div>
-                <div className="menu">
-                <button className="dropbtn">Menu</button>
-                    <div className="menu_content">
-                        <div className="notif"><a href="https://youtu.be/yPYZpwSpKmA"><h2>Notifications</h2></a></div>
-                        <div className="about"><a href="/"><h2>About</h2></a></div>
-                        <div className="friends"><a href="/"><h2>Friends</h2></a></div>
+                <div className="rightside-navbar">
+                    <div className="menu">
+                    <button className="dropbtn">Menu</button>
+                        <div className="menu_content">
+                            <div className="notif"><a href="https://youtu.be/yPYZpwSpKmA"><h2>Notifications</h2></a></div>
+                            <div className="about"><a href="/"><h2>About</h2></a></div>
+                            <div className="friends"><a href="/"><h2>Friends</h2></a></div>
+                        </div>
                     </div>
-                </div>
-                <div className="login"><a href="https://youtu.be/dQw4w9WgXcQ"><h2>Login</h2></a></div> 
+                <div className="login"><a href="https://youtu.be/dQw4w9WgXcQ"><h2>Login</h2></a></div>
+                </div> 
                 {/*<div className="notif"><a href="https://youtu.be/yPYZpwSpKmA"><h2>Notifications</h2></a></div>
                 <div className="about"><a href="/"><h2>About</h2></a></div>
                 <div className="friends"><a href="/"><h2>Friends</h2></a></div>
