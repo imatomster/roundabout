aded67064732f87bf4fcc3fbc7a8eeef7b8a3adb
Tom <imatomster@gmail.com>
2021-07-11 20:32:06 -0700

    banner

--- frontend/src/components/Description/Descript.js
+++ frontend/src/components/Description/Descript.js
@@ -4,7 +4,9 @@ import './Descript.css'
 const Descript = () => {
     return (
         <div className="description">
-            <p>Welcome to Roundabout! An application to locate your closest friends & family. Easily facilitate gatherings and be up to date on arrival times.</p>
+            <div className="textBox">
+                <p>Welcome to Roundabout! An application to locate your closest friends & family. Easily facilitate gatherings and be up to date on arrival times.</p>
+            </div>
         </div>
     )
 }
